7eb1517fd975ad1fa9ce2e68a120f014af55a0c2
Rahul Rama Chandran <rahulrchandran00@gmail.com>
2023-01-09 10:49:59 +0530

    updated Desktop notification module

--- desktop_notification.py
+++ desktop_notification.py
@@ -4,8 +4,8 @@ from plyer import notification
 if __name__ == "__main__":
     while True:
         notification.notify(
-            title   = "ALERT!!!",
-            message = "Take a break! It has been an hour!"
+            title = "ALERT!!!",
+            message = "Take a break! It has been an hour!",
             timeout = 10
         )
         time.sleep(3600)
